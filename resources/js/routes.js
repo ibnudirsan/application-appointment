8d49a356109c667471dab01d7f3c89e4ee295978
IDrumahdev <ibnudirsan@gmail.com>
2024-05-04 13:32:37 +0700

    setting router vue

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,37 @@
-import Dashboard from "../components/Dashboard.vue"
+import Dashboard from "./components/Dashboard.vue";
+import ListApointments from "./pages/apointments/apointments.vue";
+import ListUsers from "./pages/users/ListUser.vue";
+import ListSettings from "./pages/settings/UpdateSetting.vue";
+import UpdateProfile from "./pages/profile/UpdateProfile.vue";
 
 export default [
     {
         path: '/admin/dashboard',
         name: 'admin.dashboard',
         component: Dashboard
+    },
+
+    {
+        path: '/admin/apointments',
+        name: 'admin.apointments',
+        component: ListApointments
+    },
+
+    {
+        path: '/admin/users',
+        name: 'admin.users',
+        component: ListUsers
+    },
+
+    {
+        path: '/admin/settings',
+        name: 'admin.settings',
+        component: ListSettings
+    },
+
+    {
+        path: '/admin/profile',
+        name: 'admin.profile',
+        component: UpdateProfile
     }
 ]
