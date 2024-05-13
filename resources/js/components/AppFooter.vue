e461d650933e1e6b132c5785eb0336d3ef8ee743
IDrumahdev <ibnudirsan@gmail.com>
2024-05-14 04:47:09 +0700

    48_Creating Sub Components, Using Props

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    setting: Object
+})
+</script>
+<template>
+    <footer class="main-footer">
+        <div class="float-right d-none d-sm-inline">
+            Anything you want
+        </div>
+        <strong>Copyright &copy; 2024-2021 <a href="#">{{ setting?.app_name }}</a>.</strong> All
+        rights reserved.
+    </footer>
+</template>
